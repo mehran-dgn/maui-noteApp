010727f500114ace3112ca71ae43ef6117d21e3d
mehran-dgn <mehrandehghanan@yahoo.com>
2024-10-12 09:54:16 +0330

    Notes page added , now each note can open in its individual page for edit or delete , models added for the notes  - project finished

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -1,4 +1,4 @@
-namespace Notes;
+namespace Notes.Views;
 
 public partial class AboutPage : ContentPage
 {
@@ -9,6 +9,8 @@ public partial class AboutPage : ContentPage
 
 	private async void LearnMore_Clicked(object sender , EventArgs e)
 	{
-		await Launcher.Default.OpenAsync("http://blogfa.com");
+		if (BindingContext is Models.About about) {
+			await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+		}
 	}
 }
